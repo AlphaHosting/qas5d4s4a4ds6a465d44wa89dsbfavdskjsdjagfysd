7984d6f906d98893e81237bfe9e30d85a85e7977
AlphaHosting <42865213+AlphaHosting@users.noreply.github.com>
2018-09-11 02:12:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const prefix = '.'
 
 client.on('ready', function(){	
     var ms = 40000 ;
-    var setGame = ['.help','${client.guilds.size} Server','${client.users.size} User'];	
+    var setGame = ['.help','Quran Bot'];	
     var i = -1;	
     var j = 0;	
     setInterval(function (){	
