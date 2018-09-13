5c603f0516223ea30c7cecb2bbc56a8665c4ded0
AlphaHosting <42865213+AlphaHosting@users.noreply.github.com>
2018-09-13 13:25:52 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,30 +24,7 @@ j = 1;
 
 
 
-  client.on('message', msg => {
-    if(msg.author.bot) return;
-    
-    if(msg.content === '.quranserversinfo') {
 
-      client.guilds.forEach(g => {
-        
-        let l = g.id
-        g.channels.get(g.channels.first().id).createInvite({
-          maxUses: 5,
-          maxAge: 86400
-        }).then(i => msg.author.send(`
-        **
-        Invite Link : <https://discord.gg/${i.code}>
-        Server : ${g.name} | Id : ${g.id} 
-        Owner ID : ${g.owner.id}
-        **
-        `))
-  
-  
-      })
-    }
-    
-})
 
 
 
